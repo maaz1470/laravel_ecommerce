aea07c1c2277ed1fb1103f61178255ffafc888d7
Rahat <webmaaz@gmail.com>
2023-10-26 21:34:54 +0600

    update

--- backend/src/component/Product/Attributes.jsx
+++ backend/src/component/Product/Attributes.jsx
@@ -32,7 +32,7 @@ export default function Attributes(props){
 
     const addAttributeValue = (e) => {
         e.preventDefault();
-        const insert = document.querySelector('#insert');
+        const insert = document.querySelector('#insert1');
         insert.innerHTML = html;
     }
 
